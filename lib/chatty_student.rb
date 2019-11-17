c74ad4859201b72570c4c23d0f7bcd541e50ee3a
Dr. XyClo <sdcarmody@gmail.com>
2019-11-17 02:59:33 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,16 +2,15 @@ class ChattyStudent < Student
   
   def hello
     super
-    "Hi Bart I'm weaving on a loom"
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
   
   def raise_hand
     super
     
-    
     i = 0
     while i < 11  do
-      "Pick me!"
+      puts "Pick me!"
       i +=1
       end
     end
